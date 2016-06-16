b13f3568a20b737995af248c3249545e8fa3d15a
Christopher TruLove <c.t.trulove@gmail.com>
2016-06-15 20:23:52 -0700

    ctt : Another session of tinkering, set up the nescessary functions (I think) and added some more ui logic

---
+++ api/get_list.php
@@ -0,0 +1,79 @@
+							<table class="table table-striped table-hover table-condensed eatery-table" style="table-layout:fixed">
+								<tr>
+									<td>Good Restaurant</td>
+									<td>123 Address Street<br>Phoenix, AZ 85000</td>
+									<td><a href="#"><img src="http://maps.googleapis.com/maps/api/staticmap?center=4413%20e%20windsong%20dr%20phoenix,%20az%2085048&size=64x64&key=ABQIAAAAvwR4dZG84g_plzGO1oAx9BRFvf734m_1IckaSObe5rel7hil9RR9SqeNV8FaFxAHjh75woIeU0g4vQ"></a></td>
+									<td class="success">5</td>
+									<td class="danger">5</td>
+								</tr>
+								<tr>
+									<td>Another Place</td>
+									<td>987 Location Drive<br>Phoenix, AZ 85000</td>
+									<td><a href="#"><img src="http://maps.googleapis.com/maps/api/staticmap?center=4413%20e%20windsong%20dr%20phoenix,%20az%2085048&size=64x64&key=ABQIAAAAvwR4dZG84g_plzGO1oAx9BRFvf734m_1IckaSObe5rel7hil9RR9SqeNV8FaFxAHjh75woIeU0g4vQ"></a></td>
+									<td class="success">2</td>
+									<td class="danger">7</td>
+								</tr>
+								<tr>
+									<td>Another Place</td>
+									<td>987 Location Drive<br>Phoenix, AZ 85000</td>
+									<td><a href="#"><img src="http://maps.googleapis.com/maps/api/staticmap?center=4413%20e%20windsong%20dr%20phoenix,%20az%2085048&size=64x64&key=ABQIAAAAvwR4dZG84g_plzGO1oAx9BRFvf734m_1IckaSObe5rel7hil9RR9SqeNV8FaFxAHjh75woIeU0g4vQ"></a></td>
+									<td class="success">2</td>
+									<td class="danger">7</td>
+								</tr>
+								<tr>
+									<td>Another Place</td>
+									<td>987 Location Drive<br>Phoenix, AZ 85000</td>
+									<td><a href="#"><img src="http://maps.googleapis.com/maps/api/staticmap?center=4413%20e%20windsong%20dr%20phoenix,%20az%2085048&size=64x64&key=ABQIAAAAvwR4dZG84g_plzGO1oAx9BRFvf734m_1IckaSObe5rel7hil9RR9SqeNV8FaFxAHjh75woIeU0g4vQ"></a></td>
+									<td class="success">2</td>
+									<td class="danger">7</td>
+								</tr>
+								<tr>
+									<td>Another Place</td>
+									<td>987 Location Drive<br>Phoenix, AZ 85000</td>
+									<td><a href="#"><img src="http://maps.googleapis.com/maps/api/staticmap?center=4413%20e%20windsong%20dr%20phoenix,%20az%2085048&size=64x64&key=ABQIAAAAvwR4dZG84g_plzGO1oAx9BRFvf734m_1IckaSObe5rel7hil9RR9SqeNV8FaFxAHjh75woIeU0g4vQ"></a></td>
+									<td class="success">2</td>
+									<td class="danger">7</td>
+								</tr>
+								<tr>
+									<td>Another Place</td>
+									<td>987 Location Drive<br>Phoenix, AZ 85000</td>
+									<td><a href="#"><img src="http://maps.googleapis.com/maps/api/staticmap?center=4413%20e%20windsong%20dr%20phoenix,%20az%2085048&size=64x64&key=ABQIAAAAvwR4dZG84g_plzGO1oAx9BRFvf734m_1IckaSObe5rel7hil9RR9SqeNV8FaFxAHjh75woIeU0g4vQ"></a></td>
+									<td class="success">2</td>
+									<td class="danger">7</td>
+								</tr>
+								<tr>
+									<td>Another Place</td>
+									<td>987 Location Drive<br>Phoenix, AZ 85000</td>
+									<td><a href="#"><img src="http://maps.googleapis.com/maps/api/staticmap?center=4413%20e%20windsong%20dr%20phoenix,%20az%2085048&size=64x64&key=ABQIAAAAvwR4dZG84g_plzGO1oAx9BRFvf734m_1IckaSObe5rel7hil9RR9SqeNV8FaFxAHjh75woIeU0g4vQ"></a></td>
+									<td class="success">2</td>
+									<td class="danger">7</td>
+								</tr>
+								<tr>
+									<td>Another Place</td>
+									<td>987 Location Drive<br>Phoenix, AZ 85000</td>
+									<td><a href="#"><img src="http://maps.googleapis.com/maps/api/staticmap?center=4413%20e%20windsong%20dr%20phoenix,%20az%2085048&size=64x64&key=ABQIAAAAvwR4dZG84g_plzGO1oAx9BRFvf734m_1IckaSObe5rel7hil9RR9SqeNV8FaFxAHjh75woIeU0g4vQ"></a></td>
+									<td class="success">2</td>
+									<td class="danger">7</td>
+								</tr>
+								<tr>
+									<td>Another Place</td>
+									<td>987 Location Drive<br>Phoenix, AZ 85000</td>
+									<td><a href="#"><img src="http://maps.googleapis.com/maps/api/staticmap?center=4413%20e%20windsong%20dr%20phoenix,%20az%2085048&size=64x64&key=ABQIAAAAvwR4dZG84g_plzGO1oAx9BRFvf734m_1IckaSObe5rel7hil9RR9SqeNV8FaFxAHjh75woIeU0g4vQ"></a></td>
+									<td class="success">2</td>
+									<td class="danger">7</td>
+								</tr>
+								<tr>
+									<td>Another Place</td>
+									<td>987 Location Drive<br>Phoenix, AZ 85000</td>
+									<td><a href="#"><img src="http://maps.googleapis.com/maps/api/staticmap?center=4413%20e%20windsong%20dr%20phoenix,%20az%2085048&size=64x64&key=ABQIAAAAvwR4dZG84g_plzGO1oAx9BRFvf734m_1IckaSObe5rel7hil9RR9SqeNV8FaFxAHjh75woIeU0g4vQ"></a></td>
+									<td class="success">2</td>
+									<td class="danger">7</td>
+								</tr>
+								<tr>
+									<td>Another Place</td>
+									<td>987 Location Drive<br>Phoenix, AZ 85000</td>
+									<td><a href="#"><img src="http://maps.googleapis.com/maps/api/staticmap?center=4413%20e%20windsong%20dr%20phoenix,%20az%2085048&size=64x64&key=ABQIAAAAvwR4dZG84g_plzGO1oAx9BRFvf734m_1IckaSObe5rel7hil9RR9SqeNV8FaFxAHjh75woIeU0g4vQ"></a></td>
+									<td class="success">2</td>
+									<td class="danger">7</td>
+								</tr>
+							</table>
