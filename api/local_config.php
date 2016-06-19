05793d5917373494018d93f8153caa6c29a68a12
Christopher TruLove <c.t.trulove@gmail.com>
2016-06-18 18:16:07 -0700

    ctt : sigh

--- api/local_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// These are being set in production, but not in the demo code.
-/*
-if( !isset( $sql_host ) ) $sql_host = "trulove.db.8154643.hostedresource.com";
-if( !isset( $sql_user ) ) $sql_user = "trulove";
-if( !isset( $sql_pass ) ) $sql_pass = "JabberDaddy#0";
-if( !isset( $sql_db ) ) $sql_db = "trulove";
-*/
-if( !isset($email_domains) ) $email_domains = ['trulove.cc','poolsupplyworld.com'];
